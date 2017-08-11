6ab7d122502867f49cf3ba9f3d9c1d0b57e6f72a
Emilio Quilodran <equilodran@despegar.com>
2017-08-11 19:26:53 -0300

    fix person component

--- src/app/person.service.ts
+++ src/app/person.service.ts
@@ -20,18 +20,11 @@ export class PersonService {
 			.catch(this.handleError);
 	}
 
-	getPersonTemp(i: number): Promise<User[]> {
-		return this.http.get(this.personsUrl)
-			.toPromise()
-			.then(response => response.json()[i])
-			.catch(this.handleError);
-	}
-
 getPerson(id: number): Promise<User> {
 	  const url = `${this.personsUrl}/${id}`;
 	  return this.http.get(url)
 	    .toPromise()
-	    .then(response => response.json().data as User)
+	    .then(response => response.json() as User)
 	    .catch(this.handleError);
 	}
 
