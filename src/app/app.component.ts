31aa6ed0a97da71d8cb1c7577868494a991ac0ab
Emilio Quilodran <equilodran@despegar.com>
2017-09-09 01:07:30 -0300

    se actualizo bootstrap - refactor detalle de clientes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'my-app',
 	template: `
-		<nav class="header navbar navbar-toggleable-md navbar-inverse bg-inverse">
+		<nav class="header navbar navbar-expand-lg navbar-dark bg-dark">
 			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
