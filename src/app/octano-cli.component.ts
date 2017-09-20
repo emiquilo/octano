acf12000b8f08f68c5cb26c9929f29dba684c5bc
Emilio Quilodran <equilodran@despegar.com>
2017-09-20 18:33:08 -0300

    fix

--- src/app/octano-cli.component.ts
+++ src/app/octano-cli.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
 
 import { User } from './model';
 import { Moto } from './model';
@@ -12,10 +13,35 @@ import { PersonService } from './person.service';
 
 export class CliComponent implements OnInit {
 	persons: User[];
-	selectedPerson: User;
+	logs: string[] = [];
+	commandInput = new FormControl("");
 
 	constructor(
 		private personService: PersonService,
 		private router: Router) {}
-	
+
+	inputCommand(command: string) {
+    // comandos soportados
+		var commands = [
+			/cargar-usuario (\w+) (\w+) (\d+)/,
+			/eliminar-usuario (\w+) (\w+)/
+		];
+
+		// itero por todos los comandos a ver si alguno matchea
+		// si, por ejemplo, matchea "cargar-usuario" llamamos a su respectivo handler
+	}
+
+	greeting() {
+		setTimeout(()=> {this.logs.push(" Hola pescao!")}, 5000);
+	}
+
+	ngOnInit(){
+		this.greeting();
+	}
+
+	shoot(){
+		let name = this.commandInput.value;
+		this.logs.push(name);
+	}
+
 }
